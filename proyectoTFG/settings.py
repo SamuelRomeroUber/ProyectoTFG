7897d1460ef3e66112910a09a6eded9a93de365a
SamuelRomeroUber <samueldelatorre2517@gmail.com>
2025-05-13 14:44:32 +0200

    Arreglo en los statics y principio de decoración en el home

--- proyectoTFG/settings.py
+++ proyectoTFG/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%gupg#og&oc-j&)b8kpv&%@i_czl_=n@g#-bsreaypz=+@!x*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
@@ -126,9 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # Asegúrate de tener esto:
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR, 'static'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Organiza2', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- proyectoTFG/urls.py
+++ proyectoTFG/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
+from proyectoTFG.settings import BASE_DIR
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Organiza2.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'Organiza2', 'static'))
