45a6f56ce52896ac87b9652b2eafb0f381307940
SamuelRomeroUber <samueldelatorre2517@gmail.com>
2025-05-14 15:05:46 +0200

    login y registro con css

--- proyectoTFG/settings.py
+++ proyectoTFG/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%gupg#og&oc-j&)b8kpv&%@i_czl_=n@g#-bsreaypz=+@!x*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
